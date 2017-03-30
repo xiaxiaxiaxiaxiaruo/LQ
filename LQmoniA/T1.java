22eabf1120ffd74a594317b7a5a2b77c4ca1df6b
xiaxiaxiaxiaxiaruo <ruojingzhang@163.com>
2017-03-30 12:55:17 +0800

    //枚举//暴力检测

--- LQmoniA/T1.java
+++ LQmoniA/T1.java
@@ -1,17 +1,14 @@
-package LQMoNiA;
-
-
 public class T1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         int a;
-         for(a=0;a<200;a++){
-        	 if(a*a-a>=1800&&a*a-a<1900){
-        		 break;
-        	 }
-         }
-    	 System.out.printf("%d\n%d", a,a*a-a);
+		int a;// 枚举年龄//暴力检测
+		for (a = 0; a < 200; a++) {
+			if (a * a - a >= 1800 && a * a - a < 1900) {
+				break;
+			}
+		}
+		System.out.printf("%d\n%d", a, a * a - a);
 
 	}
 
